4baba8d38f7df3d8efbb92ceb421f610394033f2
Felix Wittwer <felix.wittwer@gmail.com>
2016-03-31 20:13:45 +0200

    Add rudimentary models and simple tests

--- web/models.py
+++ web/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tdo(models.Model):
+    tdo_id = models.IntegerField()  # TODO make unique?
+    name = models.CharField(max_length=250)
+    done = models.BooleanField(default=False)
+    owner = models.ForeignKey(
+        'TdoUser',
+        on_delete=models.CASCADE,
+    )
+
+
+class TdoUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tokens = models.TextField(default="")
+
+    @staticmethod
+    def create(
+        username,
+        email,
+        password,
+        first_name,
+        last_name
+    ):
+        user = User.objects.create_user(
+            email=email,
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+        )
+        user.is_active = False
+        user.save()
+
+        return TdoUser.objects.create(user=user)
+
+    def add_token(self, token):
+        pass  # TODO method to add a new token
+
+    def get_tokens(self):
+        # return self.tokens.split()
+        pass  # TODO method to get a list of tokens
--- web/tests.py
+++ web/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from web.models import *
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TdoUserTestCase(TestCase):
+    def setUp(self):
+        testuser = TdoUser.create(
+            username='foobar',
+            email='foo@bar.com',
+            password='1234123412341324',
+            first_name='Foo',
+            last_name='Bar'
+        )
+        tdo1 = Tdo.objects.create(
+            tdo_id=1,
+            name='This is a test.',
+            owner=testuser)
+        tdo2 = Tdo.objects.create(
+            tdo_id=2,
+            name='This is another test.',
+            owner=testuser)
+
+    def test_user_creation(self):
+        """Creating Users works as expected."""
+        user1 = TdoUser.objects.get(user=User.objects.get(username='foobar'))
+        self.assertEqual(user1.user.username, 'foobar')
+
+    def test_tdo_addition(self):
+        user1 = TdoUser.objects.get(user=User.objects.get(username='foobar'))
+        tdo1 = Tdo.objects.get(tdo_id=1)
+        self.assertEqual(tdo1.name, 'This is a test.')
