b91a661958cd542dc0f22bebc19e84f5a4a6f96d
Felix Wittwer <felix.wittwer@gmail.com>
2016-04-04 09:34:24 +0200

    Migrate databases and add simple test view

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Tdo)
+admin.site.register(TdoUser)
+admin.site.register(TdoList)
--- web/views.py
+++ web/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the websites index.")
+
+
+def test(request):
+    context = {
+        'content': ['Test-String', 1, 'Wow']
+    }
+    return render(request, 'test.html', context)
